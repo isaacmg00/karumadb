ecddd9589fce1412aab7dcf8788ee77e67d49e52
Isaac Garcia <isaacmg123@gmail.com>
2023-09-26 02:38:42 -0700

    finalizing frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'category', component: SingleCategoryComponent},
   {path: 'post', component: SinglePostComponent},
+  {path: 'about', component: AboutComponent},
 
   {path: 'contact', component: ContactUsComponent},
 ];
